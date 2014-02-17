90dc08d9c9fc93bc6780d37183fc5e39da989c51
zharui <zharui@semidata.com>
2014-02-17 09:57:36 +0800

    initial sdm project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sdm::Application.config.secret_key_base = '3defac3d4eb17ebcddb5deff9b1ff8f50a4164302145e29ac6704e8906aafe77b0415a66fce91016639e78eab3393e69cec2b6d3a0ff7521e1daee7296e8b488'
